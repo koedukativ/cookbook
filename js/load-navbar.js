535aed8d9c4e11e6ff7fcc7517dad67926ff0f6b
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-09 18:15:30 +0200

    css styling and template sample

---
+++ js/load-navbar.js
@@ -0,0 +1,20 @@
+//XMLHttpRequest loads the Navbar
+var xhr = new XMLHttpRequest();
+xhr.open('GET', './navigation.html', true);
+xhr.onload = function() {
+    if(this.status == 200){
+        document.getElementById("cb-menu-overlay").innerHTML =this.responseText;
+    }
+}
+
+window.onload = function () {
+  xhr.send();
+  //Share Button implementation
+  document.getElementById("cb-share-recipe").addEventListener("click", shareURL);
+  document.querySelector(".cb-caption").textContent = document.querySelector("h1").textContent;
+}
+
+
+function shareURL() {
+    alert("Use this link to share the recipe: " + location);
+}
